be02559fb0a19e42bd9d58ce3977585b8de76a39
CodingPremkiran <88441686+CodingPremkiran@users.noreply.github.com>
2023-01-03 18:01:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+# importing modules from flask library
+from flask import Flask , render_template
+
+# creating instance of class Flask, by providing __name__ keyword as argument
+app = Flask(__name__)
+
+# write the routes using decorator functions
+# default route or 'URL'
+@app.route("/Mine")
+def home():
+
+    name = "Prem"
+    age = "14" 
+
+    return render_template('index.html' , name = name , age = age)
+
+# define the route to father webpage
+@app.route("/father")
+
+def father():
+    name = "Max"
+    age = "40"
+
+    return render_template('father.html' , name1 = name , age1 = age)
+
+# define the route to mother webpage
+@app.route("/mom")
+
+def mother():
+    name = "Emily"
+    age = "37 "
+    return render_template('mother.html' , name1 = name , age1 = age)
+
+
+
+
+
+
+# add other routes, if you want
+@app.route("/brother")
+
+def friend():
+    name = "Kyle"
+    age = "23"
+
+
+    return render_template('brother.html' , name1 = name , age1 = age)
+
+
+
+
+
+# run the file
+if __name__  ==  '__main__':
+    app.run(debug=True)
